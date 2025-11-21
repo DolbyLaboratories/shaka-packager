87fa1058ed16012de9ab39a0144765ea284a4ec8
Gregory McGarry <gregory.mcgarry@dolby.com>
2025-11-22 09:06:37 +1100

    Fix lint failure.

--- packager/app/packager_main.cc
+++ packager/app/packager_main.cc
@@ -544,7 +544,8 @@ std::optional<PackagingParams> GetPackagingParams() {
       absl::GetFlag(FLAGS_hls_media_sequence_number);
   hls_params.start_time_offset = absl::GetFlag(FLAGS_hls_start_time_offset);
   hls_params.create_session_keys = absl::GetFlag(FLAGS_create_session_keys);
-  hls_params.strict_codecs_signaling = absl::GetFlag(FLAGS_strict_codecs_signaling);
+  hls_params.strict_codecs_signaling =
+      absl::GetFlag(FLAGS_strict_codecs_signaling);
 
   TestParams& test_params = packaging_params.test_params;
   test_params.dump_stream_info = absl::GetFlag(FLAGS_dump_stream_info);
