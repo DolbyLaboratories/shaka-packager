730f887cd81d0e15343baf77726858f653047d63
Gregory McGarry <gregory.mcgarry@dolby.com>
2025-11-24 16:44:42 +1100

    Fix line lengths. Don't reference supplemental_codec_ before initialisation.

--- packager/hls/base/media_playlist.cc
+++ packager/hls/base/media_playlist.cc
@@ -388,12 +388,14 @@ bool MediaPlaylist::SetMediaInfo(const MediaInfo& media_info) {
 
   if (media_info.has_video_info()) {
     stream_type_ = MediaPlaylistStreamType::kVideo;
-    codec_ = hls_params_.strict_codecs_signaling ?
-        media_info.video_info().codec() : AdjustVideoCodec(media_info.video_info().codec()); 
+    codec_ = hls_params_.strict_codecs_signaling
+        ? media_info.video_info().codec()
+        : AdjustVideoCodec(media_info.video_info().codec()); 
     if (media_info.video_info().has_supplemental_codec() &&
         media_info.video_info().has_compatible_brand()) {
-      supplemental_codec_ = hls_params_.strict_codecs_signaling ?
-          media_info.video_info().supplemental_codec() : AdjustVideoCodec(supplemental_codec_);
+      supplemental_codec_ = hls_params_.strict_codecs_signaling
+        ? media_info.video_info().supplemental_codec()
+        : AdjustVideoCodec(media_info.video_info().codec());
       compatible_brand_ = static_cast<media::FourCC>(
           media_info.video_info().compatible_brand());
     }
