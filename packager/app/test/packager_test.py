94fd2dcdc2056fc9fb15be631f49f7620d7949e5
Yuxin Fan <yuxin.fan@dolby.com>
2025-12-02 16:40:53 +0800

    Restore code

--- packager/app/test/packager_test.py
+++ packager/app/test/packager_test.py
@@ -133,6 +133,14 @@ class DiffFilesPolicy(object):
 
       output = self._GitDiff(expected_file, actual_file)
 
+      # If this is an MP4 file, get a better looking diff.
+      if (output and
+          os.path.splitext(actual_file)[1] in {'.mp4', '.m4s'}):
+        new_output = self._Mp4Diff(
+            out_dir, expected_file, actual_file)
+
+        output = new_output or output
+
       if output:
         failure_messages += [output.decode('utf8')]
 
